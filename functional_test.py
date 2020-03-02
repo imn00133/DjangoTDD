d5c74755d74fe61c10306f47f2e9360b934edf47
Jaehyeong Kim <imn00133@gmail.com>
2020-03-03 06:08:41 +0900

    create html main page to use template

--- functional_test.py
+++ functional_test.py
@@ -45,7 +45,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: 공작깃털 사기' for row in rows)
+			any(row.text == '1: 공작깃털 사기' for row in rows),
+			"신규 작업이 테이블에 표시되지 않는다."
 		)
 		# (x for x in xs)는 generator expression이다
 		# http://bit.ly/1iXxD18(귀도의 블로그)
